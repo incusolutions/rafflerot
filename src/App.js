8200be9d7e387985ab34667d90b8f96fe6dfe58b
jorge romero <jorgeromero@iMac-de-jorge.local>
2022-05-31 21:00:57 -0500

    env4

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function Head(){
           Conect
        </button>
 
-       <button onClick={transfer}>
+       <button onClick={transferencia}>
           Join Raffle
        </button>
 
@@ -63,103 +63,114 @@ const connectWallet = async () => {
   // window.open("https://phantom.app/", "_blank");
 };
 
-const transfer = async () => {
 
-  const resp = await window.solana.connect();
 
-  fetch(process.env.REACT_APP_APITRANSFER, {
 
-      method: "POST",
+function transferencia(){
+
+    transfer();
+     const transfer = async () => {
+
+        const resp = await window.solana.connect();
       
-      // Do NOT put your keys in a *public* HTML file. This is ONLY for testing purposes.
-      headers: {
-          'Content-Type': 'application/json',
-          'APIKeyId': process.env.REACT_APP_BLOCKAPI, // INSERT-API-KEY-ID
-          'APISecretKey': process.env.REACT_APP_BLOCKAPISECRECT // INSERT-API-SECRET-KEY
-      },
+        fetch(process.env.REACT_APP_APITRANSFER, {
+      
+            method: "POST",
+            
+            // Do NOT put your keys in a *public* HTML file. This is ONLY for testing purposes.
+            headers: {
+                'Content-Type': 'application/json',
+                'APIKeyId': process.env.REACT_APP_BLOCKAPI, // INSERT-API-KEY-ID
+                'APISecretKey': process.env.REACT_APP_BLOCKAPISECRECT // INSERT-API-SECRET-KEY
+            },
+      
+            body: JSON.stringify({
+                    'sender_public_key': resp.publicKey.toString(),
+                    'recipient_address': process.env.REACT_APP_BILLETERA,
+                    'return_compiled_transaction': true,
+                    'amount': '1000000000',
+                    'token_address':'RotMAyKDv5g1UMaUzJjBriSBqzVb3eQHopc6DjW7XTp',
+                    'network': 'devnet',
+                    //'network': 'mainnet-beta'
+                }
+            )
+      
+        }).then(res => {
+      
+            res.json().then(data => {
+      
+                console.log("Blockchain API request complete! response:", data, res);
+      
+                if (data['b58_compiled_transaction'] === undefined) {
+      
+                    if (data['error_message'] === undefined) {
+      
+                        alert("Unknown error");
+      
+                    } else {
+      
+                        alert("Blockchain API Error: " + String(data['error_message']))
+      
+                    }
+      
+                } else {
+      
+                    const transactionData = data['b58_compiled_transaction'];
+                    console.log("Unsigned transaction: ", transactionData);
+      
+                    sendRequest(transactionData);
+      
+                }
+      
+            });
+      
+      
+        });
+      
+      }
+      
+      const sendRequest = async (transactionData) => {
+      
+        //const network = "https://api.mainnet-beta.solana.com/";
+        const network = "https://explorer-api.devnet.solana.com/";
+        
+      
+        const connection = new solanaWeb3.Connection(network);
+      
+        try {
+      
+            const { signature } = await window.solana.request({
+      
+                method: "signAndSendTransaction",
+                params: {
+                     message: transactionData,
+                },
+      
+            })
+      
+            console.log("Signature! signature", signature);
+      
+            console.log("Done! You can view the transaction at https://explorer.solana.com/tx/" + signature + ". If you are using devnet, add `?cluster=devnet` to the end of the url.")
+      
+            alert("Done! See the console output for how to view the transaction.")
+      
+            await connection.confirmTransaction(signature).then(data => {
+                console.log("Done!", data);
+            });
+      
+        } catch (error) {
+      
+            alert(String(error['message']));
+      
+        }
+      
+      }
 
-      body: JSON.stringify({
-              'sender_public_key': resp.publicKey.toString(),
-              'recipient_address': process.env.REACT_APP_BILLETERA,
-              'return_compiled_transaction': true,
-              'amount': '1000000000',
-              'token_address':'RotMAyKDv5g1UMaUzJjBriSBqzVb3eQHopc6DjW7XTp',
-              'network': 'devnet',
-              //'network': 'mainnet-beta'
-          }
-      )
-
-  }).then(res => {
-
-      res.json().then(data => {
-
-          console.log("Blockchain API request complete! response:", data, res);
-
-          if (data['b58_compiled_transaction'] === undefined) {
-
-              if (data['error_message'] === undefined) {
-
-                  alert("Unknown error");
-
-              } else {
-
-                  alert("Blockchain API Error: " + String(data['error_message']))
-
-              }
-
-          } else {
-
-              const transactionData = data['b58_compiled_transaction'];
-              console.log("Unsigned transaction: ", transactionData);
-
-              sendRequest(transactionData);
-
-          }
-
-      });
 
 
-  });
+
 
 }
-
-const sendRequest = async (transactionData) => {
-
-  //const network = "https://api.mainnet-beta.solana.com/";
-  const network = "https://explorer-api.devnet.solana.com/";
-  
-
-  const connection = new solanaWeb3.Connection(network);
-
-  try {
-
-      const { signature } = await window.solana.request({
-
-          method: "signAndSendTransaction",
-          params: {
-               message: transactionData,
-          },
-
-      })
-
-      console.log("Signature! signature", signature);
-
-      console.log("Done! You can view the transaction at https://explorer.solana.com/tx/" + signature + ". If you are using devnet, add `?cluster=devnet` to the end of the url.")
-
-      alert("Done! See the console output for how to view the transaction.")
-
-      await connection.confirmTransaction(signature).then(data => {
-          console.log("Done!", data);
-      });
-
-  } catch (error) {
-
-      alert(String(error['message']));
-
-  }
-
-}
-
 
 
 export default App;
